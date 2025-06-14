4078d0e5c82a63e67e2a2f30918fe70a0cd0e81f
Aditya Kumar <142611737+Taggy01@users.noreply.github.com>
2025-06-14 08:56:35 +0530

    Add files via upload

---
+++ rock-paper-scissors.js
@@ -0,0 +1,77 @@
+let varii,vall;
+let compData;
+let winCount = 0,loseCount = 0,tieCount = 0;
+
+function show(varii){
+let computerPick = Math.random();
+    console.log(computerPick)
+
+if(computerPick < 1/3 && computerPick > 0){
+    compData = 'Rock';
+    result(varii);
+}
+else if(computerPick < 2/3 && computerPick > 1/3){
+    compData = 'Paper';
+    result(varii);
+}
+else if(computerPick < 1 && computerPick > 2/3){
+    compData = 'Scissors';
+    result(varii);
+}
+}
+function result(vall){
+if(compData === vall){
+    console.log('Tie.');
+    // alert(`You Choose ${vall} and Computer Choose ${compData}, Tie.`);
+    document.querySelector('.result').innerHTML = `You Choose ${vall} and Computer Choose ${compData}, Tie.`;
+    tieCount++;
+    count();
+    
+}
+else if(compData === 'Rock' && vall === 'Scissors'){
+    console.log('You Lose.');
+    // alert(`You Choose ${vall} and Computer Choose ${compData}, You Lose.`);
+    document.querySelector('.result').innerHTML = `You Choose ${vall} and Computer Choose ${compData}, You Lose.`;
+    loseCount++;
+    count();
+}
+else if(compData === 'Paper' && vall === 'Scissors'){
+    console.log('You Win.');
+    // alert(`You Choose ${vall} and Computer Choose ${compData}, You Win.`);
+    document.querySelector('.result').innerHTML = `You Choose ${vall} and Computer Choose ${compData}, You Win.`;
+    winCount++;
+    count();
+}
+else if(compData === 'Rock' && vall === 'Paper'){
+    console.log('You Win.');
+    // alert(`You Choose ${vall} and Computer Choose ${compData}, You Win.`);
+    document.querySelector('.result').innerHTML = `You Choose ${vall} and Computer Choose ${compData}, You Win.`;
+    winCount++;
+    count();
+}
+else if(compData === 'Scissors' && vall === 'Paper'){
+    console.log('You Lose.');
+    // alert(`You Choose ${vall} and Computer Choose ${compData}, You Lose.`);
+    document.querySelector('.result').innerHTML = `You Choose ${vall} and Computer Choose ${compData}, You Lose.`;
+    loseCount++;
+    count();
+}
+else if(compData === 'Paper' && vall === 'Rock'){
+    console.log('You Lose.');
+    // alert(`You Choose ${vall} and Computer Choose ${compData}, You Lose.`);
+    document.querySelector('.result').innerHTML = `You Choose ${vall} and Computer Choose ${compData}, You Lose.`;
+    loseCount++;
+    count();
+}
+else if(compData === 'Scissors' && vall === 'Rock'){
+    console.log('You Win.');
+    // alert(`You Choose ${vall} and Computer Choose ${compData}, You Win.`);
+    document.querySelector('.result').innerHTML = `You Choose ${vall} and Computer Choose ${compData}, You Win.`;
+    winCount++;
+    count();
+}
+}
+
+function count(){
+    document.querySelector('.count').innerHTML = `Win : ${winCount} , Lose = ${loseCount} , Tie = ${tieCount}.`;
+}
